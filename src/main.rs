f4257443140241ef489c6364f83d4d3c938c06ee
MeBeQuack <MeBeQuack@gmail.com>
2023-09-30 12:14:47 +0200

    heya

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new() 
+    .add_plugins(DefaultPlugins)
+    .add_startup_system(setup_cam)
+    .run()
+}
+
+fn setup_cam(
+    mut commands: Commands,
+){
+    commands.spawn(Camera3dBundle::default());
 }
